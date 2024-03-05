8c11c568f144c20edffaaf78deed7cbc7307dd0c
Jsravelo <jsravelo3@gmail.com>
2024-03-05 17:49:15 -0300

    Fix the function that creates the Grid

--- script.js
+++ script.js
@@ -1,32 +1,27 @@
 const container = document.querySelector('#container');
 let numOfBoxes = 16;
 
-
-
-let boxWidth = 1/numOfBoxes;
-
-let gridSize = boxWidth * numOfBoxes;
-
-
-
-
+let boxWidth = 100/numOfBoxes;
 
 function makeGrid(numOfBoxes, boxWidth) {
-
-    let gridSize = boxWidth * numOfBoxes * 100;
-
-    container.style.height = `${gridSize}vw`;
-    container.style.width = `${gridSize}vw`;
 
     for (let i = 0; i < numOfBoxes * numOfBoxes; i++) {
 
         const newDiv = document.createElement('div');
         newDiv.classList.add('box');
-        newDiv.style.width = `${boxWidth * 100}%`;
-        newDiv.style.height = `${boxWidth * 100}%`;
+        newDiv.style.width = `${boxWidth}%`;
+        newDiv.style.height = `${boxWidth}%`;
         container.appendChild(newDiv);
 
+        console.log(newDiv.style.width)
+
+
+
+        // newDiv.addEventListener('mouseover', (e) => {
+        //     e.target.style.background = 'black'
+        // })
     }
+
 }
 
 makeGrid(numOfBoxes, boxWidth)
