4f539d7ee4856db2256962bdd60a063a39856c32
Jsravelo <jsravelo3@gmail.com>
2024-03-06 18:28:25 -0300

    Add gridsize input and enter button

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 const container = document.querySelector('#container');
-let numOfBoxes = 16;
+const gridSize = document.querySelector('#grid-size')
+const enter = document.querySelector('.enter')
 
-let boxWidth = 100/numOfBoxes;
+enter.addEventListener('click', () => {
+
+    const numOfBoxes = gridSize.value;
+    const boxWidth = 100/numOfBoxes;
+
+    if (gridSize.value > 100) {
+    alert("Enter a number below 100");
+    gridSize.value = '';
+    return
+    };
+    makeGrid(numOfBoxes, boxWidth);
+    
+})
+
+
 
 function makeGrid(numOfBoxes, boxWidth) {
+
+    container.innerHTML = '';
 
     for (let i = 0; i < numOfBoxes * numOfBoxes; i++) {
 
@@ -13,15 +30,14 @@ function makeGrid(numOfBoxes, boxWidth) {
         newDiv.style.height = `${boxWidth}%`;
         container.appendChild(newDiv);
 
-        console.log(newDiv.style.width)
 
+        newDiv.addEventListener('mouseover', (e) => {
 
+            e.target.style.background = 'black';
 
-        // newDiv.addEventListener('mouseover', (e) => {
-        //     e.target.style.background = 'black'
-        // })
-    }
+        });
+
+    };
 
 }
 
-makeGrid(numOfBoxes, boxWidth)
